1022605a682bbfe890224b7e603a1d56b6b19ae4
Nicolas Zordan <nicolas.zordan.tek@gmail.com>
2019-08-16 22:02:16 +0200

    [CHIP8] src and tests directories + CMakeLists

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// chip8mulator::main.cpp
+// chip8mulator main file
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "chip8mulator" << std::endl;
+    return 0;
+}
---
+++ tests/test_main.cpp
@@ -0,0 +1,6 @@
+// chip8mulator::test_main.cpp
+// catch2 main file
+//
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
